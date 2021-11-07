d03aaf24cd39b8a47be133cd4396dc758c693678
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-11-07 14:52:55 -0300

    Add Content component on App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import Provider from './context/Provider';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Content from './Components/Content';
 
 function App() {
   return (
     <Provider>
       <div className="meals">
-        <span className="logo">TRYBE</span>
-        <object
-          className="rocksGlass"
-          type="image/svg+xml"
-          data={ rockGlass }
-        >
-          Glass
-        </object>
+        <Content />
       </div>
     </Provider>
   );
