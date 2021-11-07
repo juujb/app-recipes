48f61f9970ed973a27a6ce7b2849300c7893caf4
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-11-07 15:05:17 -0300

    2° Requisito Cria todos os elementos na tela de login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,7 +3,32 @@ import React from 'react';
 export default function Login() {
   return (
     <div>
-      <h1>Login Page</h1>
+      <h1>Login</h1>
+      <div>
+        <form>
+          <label htmlFor="email">
+            <input
+              data-testid="email-input"
+              type="email"
+              name="email"
+              defaultValue="E-mail"
+            />
+          </label>
+          <label htmlFor="password">
+            <input
+              data-testid="password-input"
+              type="password"
+              name="password"
+            />
+          </label>
+        </form>
+        <button
+          data-testid="login-submit-btn"
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
     </div>
   );
 }
