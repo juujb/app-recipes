ba9b298f8ad09b9e09d30d3e2911b26865c1af7d
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-11-08 16:10:42 -0300

    Implementa o footer nas pages solicitadas

--- src/Pages/Drinks.js
+++ src/Pages/Drinks.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 
 export default function Drinks() {
   return (
     <div>
       <Header title="Bebidas" />
+      <Footer />
     </div>
   );
 }
