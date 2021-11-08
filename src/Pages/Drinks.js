1eb7c8c3475df53a5acf37fb9f2f9271d6bbde62
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-11-08 17:40:41 -0300

    16° Requisito

--- src/Pages/Drinks.js
+++ src/Pages/Drinks.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../Components/Header';
 import AppContext from '../context/AppContext';
 
 export default function Drinks() {
-  const { setPage } = useContext(AppContext);
+  const { setPage, drinks } = useContext(AppContext);
   useEffect(() => {
     setPage('drinks');
   }, []);
@@ -11,6 +12,7 @@ export default function Drinks() {
   return (
     <div>
       <Header title="Bebidas" />
+      {drinks.length === 1 && <Redirect to={ `/bebidas/${drinks[0].idDrink}` } />}
     </div>
   );
 }
--- src/Pages/Recipes.js
+++ src/Pages/Recipes.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../Components/Header';
+import AppContext from '../context/AppContext';
 
 export default function Recipes() {
+  const { meals } = useContext(AppContext);
   return (
     <div>
       <Header title="Comidas" />
+      {console.log(meals)}
+      {meals.length === 1 && <Redirect to={ `/comidas/${meals[0].idMeal}` } />}
     </div>
   );
 }
