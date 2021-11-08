56049a3099697aa6cb54124869aefcf616150ec0
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-11-08 16:22:46 -0300

    Implementa o css exigido no requisito 20

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Footer.css';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
   return (
-    <footer data-testid="footer">
+    <footer data-testid="footer" className="footer">
       <div data-testid="drinks-bottom-btn" src={ drinkIcon }>
         <Link to="/bebidas">
           <img src={ drinkIcon } alt="Icone de bebida" />
