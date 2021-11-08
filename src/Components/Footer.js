738e59c6aed90074a4e579194f201eea7ddb228e
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-11-08 15:55:22 -0300

    Implementa o botão explorar

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
 
 function Footer() {
   return (
     <footer data-testid="footer">
       <div data-testid="drinks-bottom-btn" src={ drinkIcon }>
         <Link to="/bebidas">
-          <img src={ drinkIcon } alt="Simbolo de bebida" />
+          <img src={ drinkIcon } alt="Icone de bebida" />
+        </Link>
+      </div>
+      <div data-testid="explore-bottom-btn" src={ exploreIcon }>
+        <Link to="/explorar">
+          <img src={ exploreIcon } alt="Icone de explorar" />
         </Link>
       </div>
     </footer>
