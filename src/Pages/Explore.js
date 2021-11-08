136250dd791e91aefc754f5baec110acd1de1e31
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-11-08 19:07:13 -0300

    Iniciando req 67a 69

--- src/Pages/Explore.js
+++ src/Pages/Explore.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
-
+// Nova branch
 export default function Explore() {
   return (
     <div>
