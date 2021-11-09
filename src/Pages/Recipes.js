e4fcd305d0a9b92395617fcbf085bb7c4ed6ea10
dhenycruz <dheniarley.ds@gmail.com>
2021-11-08 21:59:04 -0300

    tela principal de comidas

--- src/Pages/Recipes.js
+++ src/Pages/Recipes.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import CardRecipes from '../Components/CardRecipes';
 import Header from '../Components/Header';
+import AppContext from '../context/AppContext';
+import { fetchMealsAll, fetchCategories, filterCategorie } from '../services/fetchMeals';
 
 export default function Recipes() {
+  const { meals, setMeals } = useContext(AppContext);
+  const [categories, setCategories] = useState([]);
+  const [toggle, setToggle] = useState('');
+  const totalArray = 12;
+  const totalCategories = 5;
+
+  async function fetch() {
+    const mealsAll = await fetchMealsAll();
+    setMeals(mealsAll);
+    setToggle('');
+  }
+
+  async function fetchCategoriesMeals() {
+    const categoriesAll = await fetchCategories();
+    setCategories(categoriesAll);
+  }
+
+  async function fetchFilterCategorie(categorie) {
+    const filterCat = await filterCategorie(categorie);
+    setMeals(filterCat);
+  }
+
+  useEffect(() => {
+    fetch();
+    fetchCategoriesMeals();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function handleClick(event, categorie) {
+    if (toggle === categorie) {
+      fetch();
+    } else {
+      fetchFilterCategorie(categorie);
+      setToggle(categorie);
+    }
+  }
+
   return (
     <div>
       <Header title="Comidas" />
+      <div>
+        <button type="button" onClick={ fetch }>All</button>
+        {
+          categories.slice(0, totalCategories).map((categorie) => (
+            <button
+              type="button"
+              key={ categorie.strCategory }
+              onClick={ (event) => handleClick(event, categorie.strCategory) }
+            >
+              { categorie.strCategory}
+            </button>
+          ))
+        }
+      </div>
+      <div style={ { display: 'flex', flexWrap: 'wrap' } }>
+        {
+          meals.slice(0, totalArray).map((meal) => (
+            <div key={ meal.idMeal }>
+              <Link to={ `comidas/${meal.idMeal}` }>
+                <CardRecipes
+                  index={ meal.idMeal }
+                  img={ meal.strMealThumb }
+                  name={ meal.strMeal }
+                />
+              </Link>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
--- src/services/fetchMeals.js
+++ src/services/fetchMeals.js
@@ -1,6 +1,7 @@
 const CATEGORIES = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 const AREAS = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
 const INGREDIENTS = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+const MEALSALL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
 export async function fetchCategories() {
   const request = await fetch(CATEGORIES);
@@ -16,6 +17,18 @@ export async function fetchAreas() {
 
 export async function fetchIngredients() {
   const request = await fetch(INGREDIENTS);
+  const data = await request.json();
+  return data.meals;
+}
+
+export async function fetchMealsAll() {
+  const request = await fetch(MEALSALL);
+  const data = await request.json();
+  return data.meals;
+}
+
+export async function filterCategorie(categorie) {
+  const request = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categorie}`);
   const data = await request.json();
   return data.meals;
 }
