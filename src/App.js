0d59a02e2ac56eb94b848b2bb8976bab4c60abed
dhenycruz <dheniarley.ds@gmail.com>
2021-11-17 18:05:22 -0300

    Estilização Página de Login.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Provider from './context/Provider';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Content from './Components/Content';
+import './App.css';
 
 function App() {
   return (
     <Provider>
       <div className="meals">
-        <Content />
+        <div className="app">
+          <Content />
+        </div>
       </div>
     </Provider>
   );
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { Button, Form, Image } from 'react-bootstrap';
 import AppContext from '../context/AppContext';
+import logo from '../images/logo.png';
 
 export default function Login({ history }) {
   const { loginInfo, handleLoginInfos } = useContext(AppContext);
@@ -23,36 +25,48 @@ export default function Login({ history }) {
   const isValid = handleValidation(email, password);
 
   return (
-    <div>
-      <h1>Login</h1>
-      <div>
-        <form>
-          <label htmlFor="email">
-            <input
+    <div className="d-flex flex-column bd-highlight mb-3">
+      <Image src={ logo } width="120" alt="Logo" className="rounded mx-auto d-block" />
+      <div className="d-flex flex-column bd-highlight mb-3">
+        <Form>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Login:</Form.Label>
+            <Form.Control
               data-testid="email-input"
               type="email"
               name="email"
+              placeholder="email@exemplo.com"
               defaultValue={ email }
+              size="sm"
               onChange={ handleLoginInfos }
             />
-          </label>
-          <label htmlFor="password">
-            <input
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Senha:</Form.Label>
+            <Form.Control
               data-testid="password-input"
               type="password"
               name="password"
+              size="sm"
+              placeholder="senha"
               onChange={ handleLoginInfos }
             />
-          </label>
-        </form>
-        <button
+            <Form.Text className="text-muted">
+              A senha deve ter no mínimo 7 caracteres.
+            </Form.Text>
+          </Form.Group>
+        </Form>
+        <Button
           data-testid="login-submit-btn"
           type="button"
           disabled={ !isValid }
           onClick={ handleSubmit }
+          size="lg"
+          className="p-2 flex-fill bd-highlight"
+          variant="primary"
         >
           Entrar
-        </button>
+        </Button>
       </div>
     </div>
   );
