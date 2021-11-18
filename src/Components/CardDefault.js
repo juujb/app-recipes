d830137babf8a8742c2e6d15301721a57e1442f1
dhenycruz <dheniarley.ds@gmail.com>
2021-11-18 16:55:16 -0300

    Estilização main da página de comidas.

--- src/Components/CardDefault.js
+++ src/Components/CardDefault.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 function CardDefault(props) {
-  const { img, name, index } = props;
+  const { img, name, index, id, category, area } = props;
+
   return (
-    <div data-testid={ `${index}-recipe-card` }>
-      <img src={ img } alt={ name } data-testid={ `${index}-card-img` } width="100" />
-      <p
-        data-testid={ `${index}-card-name` }
-      >
-        { name }
-      </p>
-    </div>
+    <Card style={ { width: '8rem' } } data-testid={ `${index}-recipe-card` }>
+      <Card.Img variant="top" src={ img } data-testid={ `${index}-card-img` } />
+      <Card.Body>
+        <Card.Title data-testid={ `${index}-card-name` }>{ name }</Card.Title>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroupItem>{ `Categoria: ${category}` }</ListGroupItem>
+        <ListGroupItem>{ `Região: ${area}` }</ListGroupItem>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href={ `/comidas/${id}` }>Mais detalhes</Card.Link>
+      </Card.Body>
+    </Card>
   );
 }
 
@@ -19,6 +26,9 @@ CardDefault.propTypes = {
   img: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  area: PropTypes.string.isRequired,
 };
 
 export default CardDefault;
