0fa79d7911b489ccdb0c9e8e68833937642b3774
Jucélio Brandão <juceneon@gmail.com>
2021-11-27 15:03:10 -0300

    Updates

---
+++ precache-manifest.7701e4dfc9210c2fd4de6197d07c178b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e9bf503a48f576110ca633a6cbf72e",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "c0cc9827110611a77cb9",
+    "url": "/app-toot-name/static/css/2.4505b1b2.chunk.css"
+  },
+  {
+    "revision": "9a7d6e498b60f2a5080d",
+    "url": "/app-toot-name/static/css/main.875c2c12.chunk.css"
+  },
+  {
+    "revision": "c0cc9827110611a77cb9",
+    "url": "/app-toot-name/static/js/2.460ea81f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/app-toot-name/static/js/2.460ea81f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7d6e498b60f2a5080d",
+    "url": "/app-toot-name/static/js/main.a971f43f.chunk.js"
+  },
+  {
+    "revision": "6be4e3e3689d610c2c14",
+    "url": "/app-toot-name/static/js/runtime-main.06dc577b.js"
+  },
+  {
+    "revision": "f46b2e060a840d08cb04d4e1099921ae",
+    "url": "/app-toot-name/static/media/avatar_profile.f46b2e06.svg"
+  },
+  {
+    "revision": "a6f71bef3bc0af3cae19697ed37e756d",
+    "url": "/app-toot-name/static/media/background.a6f71bef.jpg"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/app-toot-name/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "d4600aab592efcb0168175240ad8b480",
+    "url": "/app-toot-name/static/media/drinkIcon.d4600aab.svg"
+  },
+  {
+    "revision": "e635c9e14314575d5db777b104169414",
+    "url": "/app-toot-name/static/media/exploreIcon.e635c9e1.svg"
+  },
+  {
+    "revision": "466b3ea438313874e4b39cfaeb02acf2",
+    "url": "/app-toot-name/static/media/logo.466b3ea4.png"
+  },
+  {
+    "revision": "7e93047a9d4bd4d27de4a852c395c842",
+    "url": "/app-toot-name/static/media/mealIcon.7e93047a.svg"
+  },
+  {
+    "revision": "304a9ebc0957436a26ab8adcf5ba5863",
+    "url": "/app-toot-name/static/media/profileIcon.304a9ebc.svg"
+  },
+  {
+    "revision": "a116b69733f8fe0bb6e6b131e571e77d",
+    "url": "/app-toot-name/static/media/searchIcon.a116b697.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/app-toot-name/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/app-toot-name/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
