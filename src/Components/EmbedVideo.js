c1fe4872cd533ed499edf7e6947df86006fc0507
Jucélio Brandão <juceneon@gmail.com>
2021-11-27 15:49:41 -0300

    Fix embedVideo

--- src/Components/EmbedVideo.js
+++ src/Components/EmbedVideo.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function EmbedVideo({ url }) {
-  console.log(url);
+  const STRING_ID_LENGTH = 32;
   const allow = `accelerometer;
   autoplay;clipboard-write; encrypted-media; gyroscope; picture-in-picture`;
+  const id = url.slice(STRING_ID_LENGTH);
   return (
     <iframe
       width="360"
-      src={ url }
+      src={ `https://www.youtube.com/embed/${id}` }
       title="YouTube video player"
       frameBorder="0"
       allow={ allow }
